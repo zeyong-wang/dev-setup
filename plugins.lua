9e1c76b3ba798daf6273d4932e05948d2697eb43
zeyong wang <zeyong4c@gmail.com>
2022-12-31 15:28:28 -0500

    update

--- plugins.lua
+++ plugins.lua
@@ -3,11 +3,15 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
--- require("lsp") -- CiderLSP    
--- require("diagnostics") -- Diagnostics    
--- require("telescope_setup")    
--- require("treesitter_setup")
+require("nvim-tree-setup")
